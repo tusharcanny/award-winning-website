3dd64d54a6ef71e5c4e5b5cbddcb29a3252362d9
tusharcanny <tusharkarancan@gmail.com>
2025-04-28 11:07:05 -0500

    changed text hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -147,11 +147,11 @@ const Hero = () => {
         <div className="absolute left-0 top-0 z-40 size-full">
           <div className="mt-24 px-5 sm:px-10">
             <h1 className="special-font hero-heading text-blue-100">
-              redefi<b>n</b>e
+              Cogni<b>n</b>e
             </h1>
 
             <p className="mb-5 max-w-64 font-robert-regular text-blue-100">
-              Enter the Metagame Layer <br /> Unleash the Play Economy
+              Enter the realm of immortality <br /> Unleash the Digital Self
             </p>
 
             <Button
@@ -165,7 +165,7 @@ const Hero = () => {
       </div>
 
       <h1 className="special-font hero-heading absolute bottom-5 right-5 text-black">
-        G<b>A</b>MING
+        A<b>I</b>MING
       </h1>
     </div>
   );
