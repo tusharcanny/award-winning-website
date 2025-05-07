43de7c6ffbcabc4edb6c2208e8e7e86198e93cd7
tusharcanny <tusharkarancan@gmail.com>
2025-05-07 07:30:55 -0500

    added footer text

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,7 +12,7 @@ const Footer = () => {
     <footer className="w-screen bg-[#5542ff] py-4 text-black">
       <div className="container mx-auto flex flex-col items-center justify-between gap-4 px-4 md:flex-row">
         <p className="text-center text-sm font-light md:text-left">
-          ©Nova 2024. All rights reserved
+          Developed by Tushar Karan. All rights reserved
         </p>
 
         <div className="flex justify-center gap-4  md:justify-start">
